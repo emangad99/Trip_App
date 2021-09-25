6d972cbd8d010ff9601a4c94d26c4c255293318a
eman_gad <emangad3110@gmail.com>
2021-09-25 15:18:23 +0200

    initial commit

---
+++ app/src/main/java/com/example/trip_app/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.trip_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+ImageButton btnlogin;
+TextView txtregister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        txtregister=findViewById(R.id.txt_register);
+        btnlogin =findViewById(R.id.btn_login);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        txtregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent (MainActivity.this,RegisterActivity.class);
+
+                startActivity(registerIntent);
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/trip_app/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.example.trip_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+    }
+}
