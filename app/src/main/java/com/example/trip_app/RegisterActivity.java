23e08aad59bc703baed883b7e5e4394415214671
eman_gad <emangad3110@gmail.com>
2021-09-25 16:39:53 +0200

    initial commit

--- app/src/main/java/com/example/trip_app/RegisterActivity.java
+++ app/src/main/java/com/example/trip_app/RegisterActivity.java
@@ -2,13 +2,16 @@ package com.example.trip_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    TextView txtlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,16 @@ public class RegisterActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_register);
         getSupportActionBar().hide();
+
+        txtlogin = findViewById(R.id.txt_login);
+        txtlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent loginIntent = new Intent (RegisterActivity.this,MainActivity.class);
+
+                startActivity(loginIntent);
+            }
+        });
     }
 }
