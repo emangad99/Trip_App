7a60152a8367d6f9690f852b0574a0268e5c076c
eman_gad <emangad3110@gmail.com>
2021-09-27 01:37:15 +0200

    initial commit

--- app/src/main/java/com/example/trip_app/HomeActivity.java
+++ app/src/main/java/com/example/trip_app/HomeActivity.java
@@ -3,13 +3,9 @@ package com.example.trip_app;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -34,7 +30,6 @@ ImageView imageMenu;
 
             }
         });
-
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
             Window w = getWindow();
