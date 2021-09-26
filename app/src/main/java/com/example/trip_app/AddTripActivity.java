7410a6b3fa516d401f9e2e3dcd7e30559ea445a0
eman_gad <emangad3110@gmail.com>
2021-09-26 21:11:44 +0200

    initial commit

--- app/src/main/java/com/example/trip_app/AddTripActivity.java
+++ app/src/main/java/com/example/trip_app/AddTripActivity.java
@@ -1,13 +1,25 @@
 package com.example.trip_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddTripActivity extends AppCompatActivity {
+ImageView imagcalender;
+TextView txtcalender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,33 @@ public class AddTripActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_add_trip);
         getSupportActionBar().hide();
+
+
+        imagcalender=findViewById(R.id.imag_calender);
+        txtcalender=findViewById(R.id.txt_calender);
+
+        Calendar c = Calendar.getInstance();
+        final int year = c.get(Calendar.YEAR);
+        final int month = c.get(Calendar.MONTH);
+        final int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+        imagcalender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        AddTripActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                         month=month+1;
+                         String date=day + "/"+month+"/"+year;
+                         txtcalender.setText(date);
+
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+
+            }
+        });
     }
 }
