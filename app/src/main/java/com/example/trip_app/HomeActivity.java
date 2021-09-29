1170bd547d72fbb6767a86fa5e7438779798625a
eman_gad <emangad3110@gmail.com>
2021-09-30 00:11:36 +0200

    init commit

--- app/src/main/java/com/example/trip_app/HomeActivity.java
+++ app/src/main/java/com/example/trip_app/HomeActivity.java
@@ -2,9 +2,11 @@ package com.example.trip_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -34,8 +36,43 @@ NavigationView navigationView;
         toggle.setDrawerIndicatorEnabled(true);
         toggle.syncState();
 
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id=item.getItemId();
+
+                switch (id)
+                {
+                    case  R.id.nav_home:
+
+                        Intent intent = new Intent(HomeActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case  R.id.nav_History:
+
+                        Intent histintent = new Intent(HomeActivity.this, HistoryActivity.class);
+                        startActivity(histintent);
+                        break;
+
+                    case  R.id.nav_share:{
+
+                        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                        sharingIntent.setType("text/plain");
+                        String shareBody =  "http://play.google.com/store/apps/detail?id=" + getPackageName();
+                        String shareSub = "Try now";
+                        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
+                        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                        startActivity(Intent.createChooser(sharingIntent, "Share using"));
+
+                    }
+                    break;
 
 
+                }
+                return false;
+            }
+        });
 
         imagadd=findViewById(R.id.img_add);
         imagadd.setOnClickListener(new View.OnClickListener() {
@@ -58,4 +95,6 @@ NavigationView navigationView;
         super.onBackPressed();
         }
     }
+
+
 }
